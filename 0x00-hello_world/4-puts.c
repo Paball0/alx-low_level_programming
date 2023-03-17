a63c3f7febbf5f25f55d1bdafeffe4fced5db2da
Paball0 <paballomiya@yahoo.com>
2023-03-16 19:13:14 -0700

    4 Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
-
 /**
-
- * main - Entry point
-
+ * main - Program that uses puts function
  *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-
 {
-
-        puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
-
 }

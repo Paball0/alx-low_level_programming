c10cb3f1758c86ee9fc69303f7e69aa70b65ebb5
Paball0 <paballomiya@yahoo.com>
2023-03-17 17:29:27 -0700

    6 All single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints all single digits of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}

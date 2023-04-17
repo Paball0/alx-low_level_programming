0143f58c08c6f7342ca676c989f33c5fb4a77486
Paball0 <paballomiya@yahoo.com>
2023-04-17 10:13:18 -0700

    task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+/* _0_OBJECT_LIKE_MACRO_H_ 
+ * 
+ * header file describes macros size and name to 1024
+ *
+ */
+
+#define SIZE 1024
+
+#endif 
